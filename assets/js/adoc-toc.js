bd4338189c67f4bcf9e62af9dcf0781a4c4ab193
Anton Strogonoff <anton@strogonoff.name>
2019-02-06 18:54:29 +0800

    feat: Auto-generate in-page ToC in docs navigation

---
+++ assets/js/adoc-toc.js
@@ -0,0 +1,92 @@
+(function () {
+
+  const HEADER_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+  /* Given a container of AsciiDoc .sectN elementss,
+   * returns an object containing navigation structure like this:
+   * { items: [ { title: "Some title", path: "./#some-title", items: [ ...subitems ] }, ... }
+   * Works recursively through nested sections.
+   */
+
+  function getAdocTocItems(containerEl, sectLvl) {
+    sectLvl = sectLvl || 1;
+    var items = [];
+
+    const topLevelSections = Array.from(containerEl.children).filter((el) => {
+      return el.matches(`div.sect${sectLvl}`);
+    });
+
+    for (let sectionEl of topLevelSections) {
+      const headerEl = Array.from(sectionEl.children).filter((el) => {
+        for (let hTagName of HEADER_TAGS) {
+          if (el.matches(hTagName)) {
+            return true;
+          }
+        }
+        return false;
+      })[0];
+
+      let subItems;
+      const sectionBody = sectionEl.querySelector('div.sectionbody');
+      if (sectionBody) {
+        subItems = getAdocTocItems(sectionBody, sectLvl + 1);
+      } else {
+        subItems = [];
+      }
+
+      items.push({
+        title: headerEl.innerText,
+        path: `./#${headerEl.getAttribute('id')}`,
+        items: subItems,
+      });
+    }
+
+    return items;
+  }
+
+  /* Given a list of navigation items, returns an <ul> containing items recursively
+   * with CSS classes and layout consistent with docs navigation markup expected by the theme.
+   */
+  function formatSubItems(tocItems) {
+    const subItemContainer = document.createElement('ul');
+
+    subItemContainer.classList.add('in-page-toc');
+    subItemContainer.classList.add('nav-items');
+    subItemContainer.classList.add('subitems');
+
+    for (let item of tocItems) {
+      let itemEl, itemTitleEl, itemLinkEl;
+
+      itemEl = document.createElement('li');
+      itemEl.classList.add('item');
+
+      itemTitleEl = document.createElement('div');
+      itemTitleEl.classList.add('item-title');
+
+      itemLinkEl = document.createElement('a');
+      itemLinkEl.setAttribute('href', item.path);
+      itemLinkEl.innerText = item.title;
+
+      itemTitleEl.appendChild(itemLinkEl);
+      itemEl.appendChild(itemTitleEl);
+
+      if (item.items.length > 0) {
+        itemEl.appendChild(formatSubItems(item.items));
+      }
+
+      subItemContainer.appendChild(itemEl);
+    }
+
+    return subItemContainer;
+  }
+
+  const articleBody = document.querySelector('main section article .body');
+  const selectedItem = document.querySelector('main .docs-nav .nav-items .item.selected');
+
+  if (articleBody && selectedItem) {
+    const items = getAdocTocItems(articleBody);
+    const ulEl = formatSubItems(items);
+    selectedItem.appendChild(ulEl);
+  }
+
+}());
